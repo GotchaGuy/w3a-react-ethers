8830e24da586104742113bcaaf134013b5ab8661
Aljosa Makevic <aljosa100@gmail.com>
2023-03-17 15:03:58 +0100

    hotfix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { init, connectWallet, fetchBalance, accountAddress, network, handleAccountsChanged, getContractName } from "./ethers";
 
-init();
-
 function App() {
   const [connected, setConnected] = useState(false);
   const [contractName, setContractName] = useState(false);
